50fd5e996cd9343a0584598171a5baac3e55c9f8
Abdelilah Isswa <tomhary66@gmail.com>
2026-03-04 23:11:27 +0000

    add the transaction table and model

---
+++ app/Models/Transaction.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    protected $fillable = [
+        'company_id', 'account_id', 'category_id', 'type', 
+        'amount', 'description', 'date', 'invoice_id', 'bill_id'
+    ];
+
+    protected $casts = [
+        'amount' => 'decimal:2',
+        'date' => 'date',
+    ];
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function account()
+    {
+        return $this->belongsTo(Account::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function invoice()
+    {
+        return $this->belongsTo(Invoice::class);
+    }
+
+    public function bill()
+    {
+        return $this->belongsTo(Bill::class);
+    }
+}
