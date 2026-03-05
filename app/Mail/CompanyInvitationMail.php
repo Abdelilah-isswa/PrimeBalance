2075e5bdfb1e0374d0c99346a432778f1318ac63
Abdelilah Isswa <tomhary66@gmail.com>
2026-03-05 11:09:16 +0000

    add the company invitation mail

---
+++ app/Mail/CompanyInvitationMail.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class CompanyInvitationMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $company;
+    public $role;
+    public $inviterName;
+
+    public function __construct($company, $role, $inviterName)
+    {
+        $this->company = $company;
+        $this->role = $role;
+        $this->inviterName = $inviterName;
+    }
+
+    public function build()
+    {
+        return $this->subject('Invitation to join ' . $this->company->name)
+                    ->view('emails.company-invitation');
+    }
+}
